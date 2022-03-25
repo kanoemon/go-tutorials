348a081b1ff92c6262db0cb5631b01514d1ade15
kanoemon <kanoemon.cho@gmail.com>
2022-03-25 15:27:13 +0900

    Create greetings module

--- hello.go
+++ hello.go
@@ -3,9 +3,10 @@ package main
 import (
 	"fmt"
 
-	"rsc.io/quote"
+	"github.com/kanoemon/go-tutorials/greetings"
 )
 
 func main() {
-	fmt.Println(quote.Go())
+	message := greetings.Hello("kanoe")
+	fmt.Println(message)
 }
