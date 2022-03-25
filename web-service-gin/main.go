8ebc8bc45f6271d8a052680fca6431bb6259700d
kanoemon <kanoemon.cho@gmail.com>
2022-03-25 20:38:40 +0900

    id指定のgetルーティングを追加

--- web-service-gin/main.go
+++ web-service-gin/main.go
@@ -23,6 +23,7 @@ var albums = []album{
 func main() {
 	router := gin.Default()
 	router.GET("/albums", getAlbums)
+	router.GET("/albums/:id", getAlbumByID)
 	router.POST("/albums", postAlbums)
 
 	router.Run("localhost:8080")
@@ -31,6 +32,19 @@ func main() {
 // getAlbumsはJSONでalbumのデータを返す。
 func getAlbums(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, albums)
+}
+
+// getAlbumByIDは指定されたIDのalbumのデータを返す。
+func getAlbumByID(c *gin.Context) {
+	id := c.Param("id")
+
+	for _, a := range albums {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
 }
 
 // postAlbumsはalbumのデータを追加する。
